24b2ffa51dc0683c5fcbe64d437939f7422b9baa
Renato01011 <40170602+Renato01011@users.noreply.github.com>
2020-12-20 21:31:29 -0500

    Añadiendo Color

--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
 #include "CImg.h"
 #include "octree.h"
-#include <dirent.h>
+#include "dirent.h"
 #include <string>
+#include <tuple>
 #include <vector>
 #include <algorithm>
 #include <iostream>
 
 string lista[40];
-std::vector<std::vector<std::vector<int>>> cubo = {};
+std::vector<std::vector<std::vector<std::tuple<int,int,int>>>> cubo = {};
 
 using namespace std;
 using namespace cimg_library;
-
-
 
 void listdir(string initStrPath){
     struct dirent *dir;
@@ -37,72 +36,26 @@ void listdir(string initStrPath){
     }
 }
 
-
-CImg<int> binarize(CImg<float> &img, int umbral) {
-    CImg <int> R(img.width(),img.height());
-    for(int i=0;i< img.width();i++) {
-        for (int j = 0; j < img.height(); j++) {
-            int r = img(i, j, 0);
-            int g = img(i, j, 1);
-            int b = img(i, j, 2);
-            if ((r + g + b)*10 > umbral) {
-                R(i, j) = 1;
-            } else {
-                R(i, j) = 0;
-            }
-        }
-    }
-    return R;
-}
-/*
-int QuadTree::getBasicColor(int color){
-   if(color >= 190){
-       return 255;
-   }
-   else if(color < 190 && color >= 63){
-       return 125;
-   }
-   else {
-       return 0;
-   }
-}
-
-
-
-CImg<int> QuadTree::binarizeColors(CImg<float>& img){
-   CImg<int> imgResult(img.width(),img.height(), 1, 3, 0);
-   cimg_forXY(img, x, y){
-           int R = (int)img(x, y, 0, 0);
-           int G = (int)img(x, y, 0, 1);
-           int B = (int)img(x, y, 0, 2);
-           int rnew = getBasicColor(R);
-           int gnew = getBasicColor(G);
-           int bnew = getBasicColor(B);
-           imgResult(x, y, 0, 0) = rnew;
-           imgResult(x, y, 0, 1) = gnew;
-           imgResult(x, y, 0, 2) = bnew;
-       }
-   return imgResult;
-}*/
-
-
 int main() {
-    vector<vector<int>> val;
-    listdir("/home/jpalexander1706/CLionProjects/octree/datoscerebros/paciente1/1");
-    cubo.resize(40,val);
+    vector<vector<tuple<int,int,int>>> val;
+    listdir("D:\\Universidad\\Ciclo 6\\EDA\\Proyecto Final\\Octree\\Octree\\datos cerebros\\paciente 1\\1");
+    cubo.resize(40, val);
 
     for(auto c : lista){
         cout << c << endl;
     }
+    int sizeInBytesCube = 0;
     int counter = 0;
     for (int i = 0; i < 40; i++){
         cubo[i].resize(512);
         CImg <float> img(lista[i].c_str());
-        CImg <int> img_F = binarize(img, 3825);
         for(int j= 0; j< 512 ; j++ ){
             cubo[i][j].resize(512);
             for(int k = 0; k < 512; k++){
-                cubo[i][j][k] = img_F(j,k);
+                std::get<0>(cubo[i][j][k]) = img(j, k, 0);
+                std::get<1>(cubo[i][j][k]) = img(j, k, 1);
+                std::get<2>(cubo[i][j][k]) = img(j, k, 2);
+                sizeInBytesCube += sizeof(cubo[i][j][k]);
             }
         }
     }
@@ -110,9 +63,25 @@ int main() {
     Octree octree;
 
     octree.insert(cubo);
-    octree.split_costado(0,2 , 511, 2);
-    get_Corte(cubo, 0, 2, 511, 2);
-    //octree.split_frente(0,39 , 511, 0);
+
+    octree.split_costado(0, 0, 511, 20);
+    //octree.split_costado(0, 0, 511, 39);
+    //octree.split_costado(14, 10, 342, 11);
+    //octree.split_costado(500, 10, 100, 6);
+    //octree.split_costado(311, 7, 51, 4);
+    //octree.split_costado(1, 20, 400, 19);
+
+    get_Corte(cubo, 0, 0, 511, 20);
+    //get_Corte(cubo, 0, 0, 511, 39);
+    //get_Corte(cubo, 14, 10, 342, 11);
+    //get_Corte(cubo, 500, 10, 100, 6);
+    //get_Corte(cubo, 311, 7, 51, 4);
+    //get_Corte(cubo, 1, 20, 400, 19);
+
+    octree.split_frente(0, 40, 511, 0);
+
+    cout << "Tamaño en Bytes del cubo = " << sizeInBytesCube << " bytes" << endl;
+    cout << "Tamaño en Bytes del octree = " << octree.getsizeInBytes() << " bytes" << endl;
 
     return 0;
 }
--- octree.h
+++ octree.h
@@ -1,32 +1,39 @@
 #include <vector>
 #include <iostream>
+#include <tuple>
 #include "CImg.h"
-
 using namespace std;
 
-void get_Corte(vector<vector<vector<int>>> cubo, int x1,int z1,int x2,int z2){
+void get_Corte(vector<vector<vector<tuple<int, int, int>>>> cubo, int x1,int z1,int x2,int z2){
 
     double m;
     if(x2 == x1)m=0;
     else
         m = ( (double)z2 - (double)z1) / ((double)x2 - (double)x1);
-    printf("%f",m);
-    //X = m . Z + c
 
-    cimg_library::CImg<unsigned char> img2(512, 512);
+    clock_t tStart, tEnd;
+    double timeTakenInget_Corte;
+
+    cimg_library::CImg<float> img2(511, 511, 1, 3);
     double c = z1 -(m * x1);
+
+    tStart = clock();
     for(int i = 0; i < cubo.size(); i++){
         for(int j = 0; j < cubo[i].size(); j++){
             for(int k = 0; k < cubo[i][j].size(); k++){
-                //cout << i << " " << (int)m*j+c << endl;
                 if(i == (int)(m*j+c)){
-                        cout << "Hola";
-                        //cout << c->x << " " << c->y << " " << c->z << " " << c->color << endl;
-                        img2(j, k) = cubo[i][j][k];
+                        img2(j, k, 0) = get<0>(cubo[i][j][k]);
+                        img2(j, k, 1) = get<1>(cubo[i][j][k]);
+                        img2(j, k, 2) = get<2>(cubo[i][j][k]);
                 }
             }
         }
     }
+    tEnd = clock();
+
+    timeTakenInget_Corte = double(tEnd - tStart) / CLOCKS_PER_SEC;
+    cout << "Tiempo tomado por Get_Corte = " << timeTakenInget_Corte << " segundos" << endl;
+
     img2.display();
 }
 
@@ -35,14 +42,14 @@ class Octree {
 
 private:
 
-
-    vector<vector<vector<int>>> m;
+    int sizeInBytes = 0;
+    vector<vector<vector<tuple<int,int,int>>>> m;
 
     struct Node {
 
         int x0, xf, y0, yf, z0, zf;
         Node *children[8];
-        int color;
+        tuple<int,int,int> color;
         bool is_terminal = false;
 
         Node(int x0, int xf, int y0, int yf, int z0, int zf) : x0{x0}, xf{xf}, y0{y0}, yf{yf}, z0{z0}, zf{zf} {
@@ -59,65 +66,28 @@ private:
 
 public:
 
-    /*
-	bool SameColor(Node* &n, vector<vector<vector<int>>> m) {
-		int color = m[n->zf][n->x0][n->y0];
-
-		for (int k = n->z0; k < n->zf-1; k++) {
-			for (int i = n->x0; i < n->xf-1; i++) {
-				for (int j = n->y0; j < n->yf-1; j++) {
-					if (m[k][i][j] != color) {
-						return false;
-					}
-				}
-			}
-		}
-
-		return true;
-	}
-
-	void insert(Node* &n, vector<vector<vector<int>>>& m) {
-        std::cout << n->x0 << " " << n->xf << " " <<    n-> y0 <<" " << n->yf << " " << n->z0 << " " << n->zf << "\n";
-		if (SameColor(n, m)) {
-			n->color = m[n->zf][n->x0][n->y0];
-			n->is_terminal = true;
-			return;
-		}
-		int midX = (n->xf+n->x0)/2;
-		int midY = (n->yf+n->y0)/2;
-		int midZ = (n->zf+n->z0)/2;
-        n->children[0] = new Node(n->x0, midX, n->y0, midY, n->z0, midZ);
-        n->children[1] = new Node(midX, n->xf, n->y0, midY, n->z0, midZ);
-        n->children[2] = new Node(n->x0, midX, midY, n->yf, n->z0, midZ);
-        n->children[3] = new Node(midX, n->xf, midY, n->yf, n->z0, midZ);
-        n->children[4] = new Node(n->x0, midX, n->y0, midY, midZ, n->zf);
-        n->children[5] = new Node(midX, n->xf, n->y0, midY, midZ, n->zf);
-        n->children[6] = new Node(n->x0, midX, midY, n->yf, midZ, n->zf);
-        n->children[7] = new Node(midX, n->xf, midY, n->yf, midZ, n->zf);
-		insert(n->children[0], m);
-		insert(n->children[1], m);
-		insert(n->children[2], m);
-		insert(n->children[3], m);
-		insert(n->children[4], m);
-		insert(n->children[5], m);
-		insert(n->children[6], m);
-		insert(n->children[7], m);
-
-	}
-
-	void insert(vector<vector<vector<int>>>& m) {
-        root = new Node(0, m[0].size()-1, 0, m[0][0].size()-1, 0, m.size()-1);
-        insert(root, m);
-	}*/
+    int getsizeInBytes() { return sizeInBytes; }
 
     bool SameColor(Node *&n) {
-        int color = m[n->z0][n->x0][n->y0];
+        int umbral = 25;
+        int r = 0, b = 0, g = 0;
         for (int k = n->z0; k < n->zf; k++) {
             for (int i = n->x0; i < n->xf; i++) {
                 for (int j = n->y0; j < n->yf; j++) {
-                    if (m[k][i][j] != color) {
+                    r += get<0>(m[k][i][j]);
+                    g += get<1>(m[k][i][j]);
+                    b += get<2>(m[k][i][j]);
+                }
+            }
+        }
+
+
+        for (int k = n->z0; k < n->zf; k++) {
+            for (int i = n->x0; i < n->xf; i++) {
+                for (int j = n->y0; j < n->yf; j++) {
+                    if (abs(get<0>(m[k][i][j]) - r) > umbral || abs(get<1>(m[k][i][j]) - g) > umbral ||
+                        abs(get<2>(m[k][i][j]) - b) > umbral)
                         return false;
-                    }
                 }
             }
         }
@@ -127,40 +97,31 @@ public:
 
     void insert(Node *&n) {
         if (SameColor(n)) {
-            n->color = m[n->z0][n->x0][n->y0];
+            get<0>(n->color) = get<0>(m[n->z0][n->x0][n->y0]);
+            get<1>(n->color) = get<1>(m[n->z0][n->x0][n->y0]);
+            get<2>(n->color) = get<2>(m[n->z0][n->x0][n->y0]);
             n->is_terminal = true;
+
+            sizeInBytes += sizeof(this);
             return;
         }
         int midX = (n->xf + n->x0) / 2;
         int midY = (n->yf + n->y0) / 2;
         int midZ = (n->zf + n->z0) / 2;
-        /*if (n->x0 != midX && n->y0 != midY && n->z0 != midZ) {*/ n->children[0] = new Node(n->x0, midX, n->y0, midY,
-                                                                                             n->z0, midZ);
-        insert(n->children[0]); /*}*/
-        /*if (midX != n->xf && n->y0 != midY && n->z0 != midZ) {*/ n->children[1] = new Node(midX, n->xf, n->y0, midY,
-                                                                                             n->z0, midZ);
-        insert(n->children[1]); /*}*/
-        /*if (n->x0 != midX && midY != n->yf && n->z0 != midZ) {*/ n->children[2] = new Node(n->x0, midX, midY, n->yf,
-                                                                                             n->z0, midZ);
-        insert(n->children[2]); /*}*/
-        /*if (midX != n->xf && midY != n->yf && n->z0 != midZ) {*/ n->children[3] = new Node(midX, n->xf, midY, n->yf,
-                                                                                             n->z0, midZ);
-        insert(n->children[3]); /*}*/
-        /*if (n->x0 != midX && n->y0 != midY && midZ != n->zf) {*/ n->children[4] = new Node(n->x0, midX, n->y0, midY,
-                                                                                             midZ, n->zf);
-        insert(n->children[4]); /*}*/
-        /*if (midX != n->xf && n->y0 != midY && midZ != n->zf) {*/ n->children[5] = new Node(midX, n->xf, n->y0, midY,
-                                                                                             midZ, n->zf);
-        insert(n->children[5]); /*}*/
-        /*if (n->x0 != midX && midY != n->yf && midZ != n->zf) {*/ n->children[6] = new Node(n->x0, midX, midY, n->yf,
-                                                                                             midZ, n->zf);
-        insert(n->children[6]); /*}*/
-        /*if (midX != n->xf && midY != n->yf && midZ != n->zf) {*/ n->children[7] = new Node(midX, n->xf, midY, n->yf,
-                                                                                             midZ, n->zf);
-        insert(n->children[7]); /*}*/
+        
+        n->children[0] = new Node(n->x0, midX, n->y0, midY, n->z0, midZ); insert(n->children[0]);
+        n->children[1] = new Node(midX, n->xf, n->y0, midY, n->z0, midZ); insert(n->children[1]);
+        n->children[2] = new Node(n->x0, midX, midY, n->yf, n->z0, midZ); insert(n->children[2]);
+        n->children[3] = new Node(midX, n->xf, midY, n->yf, n->z0, midZ); insert(n->children[3]);
+        n->children[4] = new Node(n->x0, midX, n->y0, midY, midZ, n->zf); insert(n->children[4]);
+        n->children[5] = new Node(midX, n->xf, n->y0, midY, midZ, n->zf); insert(n->children[5]);
+        n->children[6] = new Node(n->x0, midX, midY, n->yf, midZ, n->zf); insert(n->children[6]);
+        n->children[7] = new Node(midX, n->xf, midY, n->yf, midZ, n->zf); insert(n->children[7]);
+
+        sizeInBytes += sizeof(this);
     }
 
-    void insert(vector<vector<vector<int>>> &m) {
+    void insert(vector<vector<vector<tuple<int, int, int>>>> &m) {
         this->m = m;
         root = new Node(0, m[0].size() - 1, 0, m[0][0].size() - 1, 0, m.size() - 1);
         insert(root);
@@ -168,13 +129,13 @@ public:
 
     struct punto {
         int x, y, z;
-        int color;
+        tuple<int,int,int> color;
 
         punto(int x, int y) : x(x), y(y) {};
 
-        punto(int x, int y, int z, int color) : x(x), y(y), z(z), color(color) {};
+        punto(int x, int y, int z, tuple<int, int, int> color) : x(x), y(y), z(z), color(color) {};
 
-        void colored(int color) { this->color = color; };
+        void colored(tuple<int,int,int> color) { this->color = color; };
     };
 
     void split_frente(int y1, int z1, int y2, int z2) {
@@ -182,17 +143,16 @@ public:
         if(y2 == y1)m=0;
         else
             m = ( (double)z2 - (double)z1) / ((double)y2 - (double)y1);
-        printf("%f",m);
-        //X = m . Z + c
 
         double c = z1 -(m * y1);
         vector<punto *> puntos;
         llenar_puntos_frente(root, puntos, m, c, y1, y2, z1, z2);
 
-        cimg_library::CImg<unsigned char> img(511, 40);
+        cimg_library::CImg<float> img(511, 40, 1, 3);
         for (auto c : puntos) {
-            //cout << c->x << " " << c->y << " " << c->z << " " << c->color << endl;
-            img(c->x, c->z) = c->color;
+            img(c->x, c->z, 0) = get<0>(c->color);
+            img(c->x, c->z, 1) = get<1>(c->color);
+            img(c->x, c->z, 2) = get<2>(c->color);
         }
         img.display();
     }
@@ -203,12 +163,8 @@ public:
                 int y_temp = i;
                 int z_temp = m * i + c;
                 if (z_temp >= node->z0 && z_temp <= node->zf) {
-                    //cout << x_temp << " " << z_temp << " " << endl;
                     for (int j = node->x0; j < node->xf; j++){
-                        //if (x_temp <= x2 and (x_temp >= x1)) {
-                        //if(y_temp <= y2 and (y_temp >= y1))
                         puntos.push_back(new punto(j, y_temp, z_temp, node->color));
-                        //}
                     }
 
                 }
@@ -241,17 +197,24 @@ public:
         if(x2 == x1)m=0;
         else
         m = ( (double)z2 - (double)z1) / ((double)x2 - (double)x1);
-        printf("%f",m);
-        //X = m . Z + c
 
         double c = z1 -(m * x1);
         vector<punto *> puntos;
-        llenar_puntos_costado(root, puntos, m, c, x1, z1, x2, z2);
 
-        cimg_library::CImg<unsigned char> img(512, 512);
+        clock_t tStart, tEnd;
+        double tiempoTomado;
+        tStart = clock();
+        llenar_puntos_costado(root, puntos, m, c, x1, z1, x2, z2);
+        tEnd = clock();
+
+        tiempoTomado = double(tEnd - tStart) / CLOCKS_PER_SEC;
+        cout << "Tiempo tomado por Split Costado = " << tiempoTomado << " segundos" << endl;
+
+        cimg_library::CImg<float> img(511, 511, 1, 3);
         for (auto c : puntos) {
-            //cout << c->x << " " << c->y << " " << c->z << " " << c->color << endl;
-            img(c->x, c->y) = c->color;
+            img(c->x, c->y, 0) = get<0>(c->color);
+            img(c->x, c->y, 1) = get<1>(c->color);
+            img(c->x, c->y, 2) = get<2>(c->color);
         }
         img.display();
     }
@@ -263,12 +226,8 @@ public:
                 int x_temp = i;
                 int z_temp = m * i + c;
                 if (z_temp >= node->z0 && z_temp <= node->zf) {
-                    //cout << x_temp << " " << z_temp << " " << endl;
                     for (int j = node->y0; j < node->yf; j++){
-                        //if (x_temp <= x2 and (x_temp >= x1)) {
-                            //if(y_temp <= y2 and (y_temp >= y1))
-                            puntos.push_back(new punto(x_temp, j, z_temp, node->color));
-                        //}
+                        puntos.push_back(new punto(x_temp, j, z_temp, node->color));
                     }
 
                 }
